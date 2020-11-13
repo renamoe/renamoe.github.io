e543fd1fabd3f1cfff90baebb5573c110a5e7ba5
renamoe <true-banana@outlook.com>
2020-11-14 01:13:04 +0800

    Site updated: 2020-11-14 01:13:04

--- dist/music.js
+++ dist/music.js
@@ -2,6 +2,8 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
+    order: 'random',
+    theme: '#ffa7e8',
     audio: [
 	    {
 	        name: 'だから僕は音楽を辞めた',
@@ -26,6 +28,18 @@ const ap = new APlayer({
 	        artist: '當山みれい',
 	        url: 'http://music.163.com/song/media/outer/url?id=488388942.mp3',
 	        cover: 'http://p2.music.126.net/kbLlBkGfEcA3RJyC5JhkDA==/18346451021830743.jpg'
+	    },
+	    {
+	        name: '花に亡霊',
+	        artist: 'ヨルシカ',
+	        url: 'http://music.163.com/song/media/outer/url?id=1442466883.mp3',
+	        cover: 'http://p1.music.126.net/_Os98d4NSKf-vLo_93QoEg==/109951164927614269.jpg'
+	    },
+	    {
+	        name: 'DAYBREAK FRONTLINE',
+	        artist: '鹿乃',
+	        url: 'http://music.163.com/song/media/outer/url?id=537854739.mp3',
+	        cover: 'http://p1.music.126.net/iLcmytETSkZgd8xcGVEO5A==/109951163200361748.jpg'
 	    }
     ]
 });
